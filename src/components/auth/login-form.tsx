ec8539f922f8426718af35d5c63ebd92cec2e74d
Shariful Islam <sajukhan12905@gmail.com>
2024-11-17 15:57:42 +0600

    Make a sign up component

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,8 +1,33 @@
+"use client";
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+//interlal import
+import {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from "../ui/form";
+import { loginSchema } from "../../../schemas";
 import CardWrappar from "./CardWrappar";
+import { Input } from "../ui/input";
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
   return (
     <>
       <CardWrappar
@@ -10,8 +35,37 @@ export const LoginForm = () => {
         backButtonHerf="/auth/register"
         backButtonLabel="Don't have account?"
         showSocial
-       >
-        Login Form
+      >
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+          </form>
+        </Form>
       </CardWrappar>
     </>
   );
