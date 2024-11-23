ccd175a821ec50bd2c1ed49fc2fd59eeeaae4577
Shariful Islam <sajukhan12905@gmail.com>
2024-11-24 00:23:14 +0600

    2:18:00

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { auth as middleware } from "../auth";
+
+export default middleware((req) => {
+  const isLoggedIn = !!req.auth
+  console.log(isLoggedIn);
+});
+
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
