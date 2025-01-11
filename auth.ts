4ee4f2c9a17aa11b0a9ccd538955006f9b4c1b9b
Shariful Islam <sajukhan12905@gmail.com>
2025-01-11 22:31:21 +0600

    time 2:20

--- auth.ts
+++ auth.ts
@@ -1,5 +1,16 @@
 import NextAuth from "next-auth";
-import Github from "next-auth/providers/github";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Github],
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,11 @@
 import { auth as middleware } from "../auth";
 
 export default middleware((req) => {
-  const isLoggedIn = !!req.auth
-  console.log(isLoggedIn);
+  const isLoggedIn = !!req.auth;
+  console.log(req.nextUrl.pathname);
 });
 
-
 export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!_next).*)"],
 };
+
